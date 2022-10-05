f6f9ae929c2f16361f2023d4557091bdc0fadcb8
nathanalves007 <110548631+nathanalves007@users.noreply.github.com>
2022-10-05 11:57:19 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -17,5 +17,17 @@ function escreva(){
 let lista = ["Terra" ,"Marte" ,"Saturno" ,"Jupter" ,"Vênus"];
 
 function mostraLista(){
-    document.write("Panela " + lista[4])
+    document.write("Tamanho da lista: " + lista.length + "<br>");
+    for(let i = 0; i <lista.length; i++){
+        document.write("Planeta " + lista[i] + "<br>");
+    }
+}
+
+function repete(){
+    for(let i = 1; i <= 10; i++){
+        document.write("Tabuada do " + i + "<br>");
+        for(let j = 1; j <=10; j++){
+            document.write(i+" x " + j + " = "+(i*j)+"<br>");
+        }
+    }
 }
