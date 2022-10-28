58c480242868290d72d6f876a70fd2421bca96f4
nathanalves007 <110548631+nathanalves007@users.noreply.github.com>
2022-10-28 12:46:27 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -24,10 +24,11 @@ function total(){
   let r = 0;
   for(let i = 1; i <= t; i++){
       r = c * (1 + (j/100));
-      document.write("Mês " + i + " valor: " + r +"<br>");
+      //document.write("Mês " + i + " valor: " + r +"<br>");
       c = r;
   }
-  document.write("Resultado: " + r);
+  document.getElementById("total").innerHTML = m;
+  //document.write("Resultado: " + r);
 }
 
 
